b095bbbef0f0c20eb5e1391f1cf7b2159dfeb696
foxqi <1067083094@qq.com>
2016-09-27 19:14:34 +0800

    zhu

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@
 var sw = new Swiper(".swiper-container", {
     loop: true,
     direction: "vertical",
-    autoplay:6000,
+    //autoplay:3000,
     autoplayDisableOnInteraction: false,
     pagination: ".swiper-pagination",
     lazyLoading: true,
